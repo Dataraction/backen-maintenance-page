6fa7774f2ff11c7de539c7d29ea666afce10cfb9
mai1015 <i@mai1015.com>
2021-01-21 15:35:59 -0500

    add redirect

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ app.use(express.static('./dist'));
 
 
 app.use(function(req, res, next){
-	res.redirect('/');
+	// res.redirect('/');
   // res.status(404);
 
-  // // respond with html page
-  // if (req.accepts('html')) {
-  //   res.render('404', { url: req.url });
-  //   return;
-  // }
+  // respond with html page
+  if (req.accepts('html')) {
+    res.redirect('/');
+    // res.render('404', { url: req.url });
+    return;
+  }
 
   // // respond with json
-  // if (req.accepts('json')) {
-  //   res.send({ error: 'Not found' });
-  //   return;
-  // }
+  if (req.accepts('json')) {
+    res.send({ error: 'maintaince mode', msg: "Sorry for the inconvenience but we’re performing some maintenance at the moment." });
+    return;
+  }
 
   // // default to plain-text. send()
-  // res.type('txt').send('Not found');
+  res.type('txt').send('Sorry for the inconvenience but we’re performing some maintenance at the moment.');
 });
 
 app.listen(8080, function () {    
